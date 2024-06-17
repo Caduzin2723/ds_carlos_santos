0a945569e0e14bb18847d62818e4803852373580
Carlos Santos <carlos2723.santos@gmail.com>
2024-06-17 09:26:27 -0300

    Inicio da Avaliação Final

---
+++ Avaliação_Final/Pessoa.java
@@ -0,0 +1,37 @@
+package Avaliação_Final;
+
+//@author Carlos Santos
+public class Pessoa {
+    private String nome = "João";
+    private int idade = 16;
+    private double altura = 1.79;
+    private String cor = "Pardo";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+}
